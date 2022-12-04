0b0d78c0fc72e5b2c16a1d2f588cd8657458b6ad
Chenzhou <171994636@qq.com>
2022-12-04 17:14:34 +0100

    Update whisper-training-pipeline.py

--- whisper-training-pipeline.py
+++ whisper-training-pipeline.py
@@ -7,7 +7,7 @@ if LOCAL == False:
    stub = modal.Stub()
    image = modal.Image.debian_slim().apt_install(["libgomp1"]).pip_install(["hopsworks==3.0.4", "seaborn", "joblib", "scikit-learn==0.24.2"])
 
-   @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("ScalableML_lab1"))
+   @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("ScalableML_lab2")) # [?]need to set modal secret on website. pay attention to the secret name
    def f():
        g()
 
@@ -35,14 +35,14 @@ def g():
     # The feature view is the input set of features for your model. The features can come from different feature groups.
     # You can select features from different feature groups and join them together to create a feature view
     try:
-        feature_view = fs.get_feature_view(name="titanic_modal", version=1)
+        feature_view = fs.get_feature_view(name="whisper_modal", version=1)
     except:
-        titanic_fg = fs.get_feature_group(name="titanic_modal", version=1)
+        titanic_fg = fs.get_feature_group(name="whisper_modal", version=1)
         query = titanic_fg.select_all()
-        feature_view = fs.create_feature_view(name="titanic_modal",
+        feature_view = fs.create_feature_view(name="whisper_modal",
                                               version=1,
-                                              description="Read from titanic dataset",
-                                              labels=["survived"],
+                                              description="Read from voice dataset",
+                                              labels=["sentence"], #sentence (string)
                                               query=query)
 
     # You can read training data, randomly split into train/test sets of features (X) and labels (y)
@@ -94,91 +94,7 @@ def g():
     # Upload the model to the model registry, including all files in 'model_dir'
     titanic_model.save(model_dir)
 
-"""
-    # My code
-    X_train = train.drop('Survived', axis=1)
-    y_train = train['Survived']
-    X_test = test.drop('Survived', axis=1)
-    y_test = test['Survived']
-    # Training
-    rs = 1
-    rf = RandomForestClassifier(random_state=rs)
-    rf.fit(X_train, y_train)
-    acc_rf = round(rf.score(X_train, y_train), 2)
 
-    # scores
-    scores_rf = cross_val_score(rf, X_train, y_train, cv=5, scoring='accuracy')
-
-    scores_results = pd.DataFrame({'Model': ['Random Forest'],
-                                   'Accuracy_score': [scores_rf.mean()],
-                                   'Standard Deviation': [scores_rf.std()]})
-    scores_results
-
-"""
-'''
-    # The feature view is the input set of features for your model. The features can come from different feature groups.    
-    # You can select features from different feature groups and join them together to create a feature view
-    try: 
-        feature_view = fs.get_feature_view(name="titanic_modal", version=1)
-    except:
-        titanic_fg = fs.get_feature_group(name="titanic_modal", version=1)
-        query = titanic_fg.select_all()
-        feature_view = fs.create_feature_view(name="titanic_modal",
-                                          version=1,
-                                          description="Read from titanic flower dataset",
-                                          labels=["variety"],
-                                          query=query)    
-
-    # You can read training data, randomly split into train/test sets of features (X) and labels (y)        
-    X_train, X_test, y_train, y_test = feature_view.train_test_split(0.2)
-
-    # Train our model with the Scikit-learn K-nearest-neighbors algorithm using our features (X_train) and labels (y_train)
-    # XGBoost for titanic
-    model = KNeighborsClassifier(n_neighbors=2)
-    model.fit(X_train, y_train.values.ravel())
-
-    # Evaluate model performance using the features from the test set (X_test)
-    y_pred = model.predict(X_test)
-
-    # Compare predictions (y_pred) with the labels in the test set (y_test)
-    metrics = classification_report(y_test, y_pred, output_dict=True)
-    results = confusion_matrix(y_test, y_pred)
-
-    # Create the confusion matrix as a figure, we will later store it as a PNG image file
-    df_cm = pd.DataFrame(results, ['True Setosa', 'True Versicolor', 'True Virginica'],
-                         ['Pred Setosa', 'Pred Versicolor', 'Pred Virginica'])
-    cm = sns.heatmap(df_cm, annot=True)
-    fig = cm.get_figure()
-
-    # We will now upload our model to the Hopsworks Model Registry. First get an object for the model registry.
-    mr = project.get_model_registry()
-    
-    # The contents of the 'titanic_model' directory will be saved to the model registry. Create the dir, first.
-    model_dir="titanic_model"
-    if os.path.isdir(model_dir) == False:
-        os.mkdir(model_dir)
-
-    # Save both our model and the confusion matrix to 'model_dir', whose contents will be uploaded to the model registry
-    joblib.dump(model, model_dir + "/titanic_model.pkl")
-    fig.savefig(model_dir + "/confusion_matrix.png")    
-
-
-    # Specify the schema of the model's input/output using the features (X_train) and labels (y_train)
-    input_schema = Schema(X_train)
-    output_schema = Schema(y_train)
-    model_schema = ModelSchema(input_schema, output_schema)
-
-    # Create an entry in the model registry that includes the model's name, desc, metrics
-    titanic_model = mr.python.create_model(
-        name="titanic_modal", 
-        metrics={"accuracy" : metrics['accuracy']},
-        model_schema=model_schema,
-        description="titanic Flower Predictor"
-    )
-    
-    # Upload the model to the model registry, including all files in 'model_dir'
-    titanic_model.save(model_dir)
-'''
 if __name__ == "__main__":
     if LOCAL == True :
         g()

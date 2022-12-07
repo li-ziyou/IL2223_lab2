ee487df7f761fd5b6f783f1296c8c9253176446e
Chenzhou <171994636@qq.com>
2022-12-07 20:32:58 +0100

    v1

--- huggingface-spaces-whisper/app.py
+++ huggingface-spaces-whisper/app.py
@@ -2,6 +2,7 @@ from transformers import pipeline
 import gradio as gr
 import os
 import subprocess
+from pytube import YouTube
 
 pipe = pipeline(model="tilos/whisper-small-zh-HK")  # change to "your-username/the-name-you-picked"
 
@@ -16,29 +17,52 @@ def transcribe(audio):
     text = pipe(audio)["text"]
     return text
 
+def get_text(url):
+    if url != '': output_text_transcribe = ''
+    result = pipe(get_audio(url))
+    return result['text'].strip()
+
+def get_audio(url):
+    website = YouTube(url)
+    video = website.streams.filter(only_audio=True).first()
+    out_file = video.download(output_path=".")
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp3'
+    os.rename(out_file, new_file)
+    audio = new_file
+    return audio
+
 def video_identity(video):
     audio_file = video2mp3(video)
     text = pipe(audio_file)#["text"]
     print(text)
     return text
+def test(a):
+    return a
 
-video = gr.Interface(video_identity,
-                    gr.Video(),
-                    "playable_video",
-                    #examples=[
-                    #    os.path.join(os.path.dirname(__file__),
-                    #                 "video/video_sample.mp4")],
-                    cache_examples=True)
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            input_mircro = gr.Audio(source="microphone", type="filepath")
+            micro_btn = gr.Button('Transcribe voice')
+        with gr.Column():
+            output_micro = gr.Textbox(placeholder='Transcript from mic.', label='Transcript') #label?
+            micro_btn.click(transcribe, inputs=input_mircro, outputs=output_micro)
 
-voice = gr.Interface(
-    fn=transcribe,
-    inputs=gr.Audio(source="microphone", type="filepath"),
-    outputs="text",
-    title="Whisper Small Cantonese",
-    description="Realtime demo for Cantonese speech recognition using a fine-tuned Whisper small model.",
-)
+    with gr.Row():
+        with gr.Column():
+            inputs_url = gr.Textbox(placeholder='Video URL', label='URL')
+            url_btn = gr.Button('Transcribe Video')
+        with gr.Column():
+            output_url = gr.Textbox(placeholder='Transcript from video.', label='Transcript')
+            url_btn.click(get_text, inputs=inputs_url, outputs=output_url)
+            examples = [["www.google.com"]]
+    with gr.Row():
+        gr.Interface(
+            fn=test,
+            inputs="audio",
+            outputs = "audio",
+            examples=[]
+        )
 
-
-
-demo = gr.TabbedInterface([video, voice])
 demo.launch()
